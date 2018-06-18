1fbb5495cca7f7dee06103aaa84ce97f225e538c
Steven Isbell <steveni_92@msn.com>
2018-06-17 22:35:16 -0500

    added profile comp

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { updateNameAction } from '../ducks/reducer';
 
 class Profile extends Component {
   constructor(props) {
@@ -10,11 +13,35 @@ class Profile extends Component {
   render() {
     return (
       <div>
-        <input onChange={this.handleNameInput} />
+        {/* updateNameAction is a function coming from redux. It will send our
+          updates back to the redux store for processing.
+        */}
+        <input onChange={this.props.updateNameAction} />
+        {/* name is a value being retrieved from the redux store */}
         {this.props.name}
       </div>
     );
   }
 }
 
-export default HelloWorld;
+// ES5
+// function mapStateToProps(state) {
+//   return state;
+// }
+
+// ES6
+const mapStateToProps = state => state;
+
+// Instead of using a variable for this object, we put it directly as the second
+// argument to our connect method
+// const mapDispatchToProps = {
+//   updateNameAction
+// }
+
+// let decorator = connect(mapStateToProps, mapDispatchToProps)
+// export default decorator(Profile);
+//This is often done in a single line of code like this:
+export default connect(
+  mapStateToProps,
+  { updateNameAction }
+)(Profile);
