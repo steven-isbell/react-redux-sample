c242263f73307fdc3e0b229cf6a78fbe347aaa68
Steven Isbell <steveni_92@msn.com>
2018-08-27 16:34:30 -0500

    added devtools link

--- src/store.js
+++ src/store.js
@@ -34,6 +34,8 @@ export default store;
 // export default store; //The store is used in index.js by the react-redux top level provider
 
 // *********** STORE WITH MULTIPLE REDUCERS AND MIDDLEWARES AND DEVTOOLS *********
+//////// Make sure to install the DEVTOOLS chrome extension here
+/////// https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd?hl=en
 
 // import { createStore, combineReducers, applyMiddleware, compose } from 'redux'; //Notice redux, not react-redux
 // import standardReducerExample from './ducks/standardReducerExample';
